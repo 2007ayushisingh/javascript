d7ccc80b08f3d92e181fc422ac86cef6b01e9de7
2007ayushisingh <123shaurya2009@gmail.com>
2026-06-19 01:19:49 +0000

    variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+//for constant values
+const accountId=53389;
+//for variables
+let name="Radhe";
+var age=25;
+accountCity="Unnao";
+// console.log(accountId);
+// console.log(name);
+// console.log(age);
+// console.log(accountCity);
+console.table({accountId,name,age,accountCity});  //*******to aaavoid writing again n again console.log */
+//*****Prefer to use let because of functional and block scopes */
